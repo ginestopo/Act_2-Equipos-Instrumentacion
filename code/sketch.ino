08b91d1441e859ebd8cf3dd0dfaee7a83c15ee72
ginestopo <ginestopo@gmail.com>
2025-05-09 12:17:08 +0200

    Update code to make it non-blocking

--- code/sketch.ino
+++ code/sketch.ino
@@ -10,8 +10,22 @@ LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
 const float GAMMA = 0.7;
 const float RL10 = 50;
 
-//Custom function declarations
-void printLCD(String name, int number = -1, int delayMS = 1000, String text = "none");
+// Function prototypes 
+void printLCD(String name, int number = -1, int delayMS = 0, String text = "none");
+void updateSensorData();
+int getLux();
+String getAirQuality();
+String getWindDirection();
+
+// Variables to avoid blocking the system
+unsigned long previousMillis = 0;
+const long displayInterval = 2000; // Screen change interval (in milliseconds)
+int currentDisplay = 0;
+byte lastTemperature = 0;
+byte lastHumidity = 0;
+int lastLux = 0;
+String lastAirQuality = "";
+String lastWindDirection = "";
 
 void setup() {
   //------- DHT22 setup -------
@@ -20,6 +34,7 @@ void setup() {
 
   //------- LCD setup ----------
   lcd.init(); // initialize the lcd 
+  lcd.backlight();
 
   //------- LDR setup -------
   pinMode(LDR_PIN, INPUT);
@@ -28,64 +43,79 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(9600);
-  
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
+  
+  // Update sensors continuously
+  updateSensorData();
+  
+  // Update the display in a non-blocking way
+  if (currentMillis - previousMillis >= displayInterval) {
+    previousMillis = currentMillis;
+    
+    switch(currentDisplay) {
+      case 0:
+        printLCD("Temperature (*C):", (int)lastTemperature);
+        break;
+      case 1:
+        printLCD("Humidity (%):", (int)lastHumidity);
+        break;
+      case 2:
+        printLCD("Luxes:", lastLux);
+        break;
+      case 3:
+        printLCD("Air quality: ", -1, 0, lastAirQuality);
+        break;
+      case 4:
+        printLCD("Wind Direction: ", -1, 0, lastWindDirection);
+        break;
+    }
+    
+    currentDisplay = (currentDisplay + 1) % 5; // Show the next param in the screen
+  }
+
+  //day/night indicator (led ON when night)
+  (lastLux > 50) ? digitalWrite(LED_BUILTIN, LOW) : digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void updateSensorData() {
   //------- DHT22 data reading ----------
   byte temperature = 0;
   byte humidity = 0;
   byte data[40] = {0};
-  if(sensorht.read(SENSORHT, &temperature, &humidity, data)){
-    Serial.println("There was an error reading DHT22!");
+  if(!sensorht.read(SENSORHT, &temperature, &humidity, data)) {
+    lastTemperature = temperature;
+    lastHumidity = humidity;
   }
-  //------- DHT22 data reading END -------
-
-  //------- LCD printing ----------
-  printLCD("Temperature (*C):",(int)temperature,2000);
-  printLCD("Humidity (%):",(int)humidity,2000);
-  printLCD("Luxes:", getLux(),2000);
-  printLCD("Air quality: ", -1, 2000,  getAirQuality());
-  printLCD("Wind Direction: ", -1, 2000,  getWindDirection());
-  //------- LCD printing END -------
-
-  //day/night indicator (led ON when night)
-  (getLux() > 50) ? digitalWrite(LED_BUILTIN, LOW) : digitalWrite(LED_BUILTIN, HIGH);
-
+  
+  // Update other sensors
+  lastLux = getLux();
+  lastAirQuality = getAirQuality();
+  lastWindDirection = getWindDirection();
 }
 
-
-/*
-###############################
-      My Custom Functions
-###############################
-*/
-
-//this function will print in the LCD display the data with its name
-void printLCD(String name, int number = -1, int delayMS = 1000, String text = "none"){
+// printLCD without delay
+void printLCD(String name, int number = -1, int delayMS = 0, String text = "none"){
   lcd.clear();
   lcd.setCursor(0, 0); 
   lcd.print(name);   
   lcd.setCursor(2, 1);
-  //Maybe some values are string, so only print number if it is not the default value 
   if(number != -1)        
     lcd.print(number);
   else
     lcd.print(text);
-  delay(delayMS);
 }
 
-//this function makes the conversion from the analog value to luxes
 int getLux(){
   int analogValue = analogRead(A0);
   float voltage = analogValue / 1024. * 5;
   float resistance = 2000 * voltage / (1 - voltage / 5);
   float lux = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
-
   return lux;
 }
 
-//this function emulates the result of using an air quality sensor
 String getAirQuality(){
   int analogValue = analogRead(A1);
   if(analogValue > 515)
@@ -94,12 +124,10 @@ String getAirQuality(){
     return "Bad :(";
 }
 
-//emulate wind direction by using a joystick
 String getWindDirection(){
-  int y = analogRead(A2); //vertical
-  int x = analogRead(A3); //horizontal
+  int y = analogRead(A2);
+  int x = analogRead(A3);
   const int DEADZONE = 100;
-
   int centerX = 512;
   int centerY = 512;
 
@@ -108,11 +136,11 @@ String getWindDirection(){
   }
 
   if (abs(x - centerX) > abs(y - centerY)) {
-    if (x > centerX + DEADZONE) return "Left";
-    if (x < centerX - DEADZONE) return "Right";
+    if (x > centerX + DEADZONE) return "West";
+    if (x < centerX - DEADZONE) return "East";
   } else {
-    if (y > centerY + DEADZONE) return "Up";
-    if (y < centerY - DEADZONE) return "Down";
+    if (y > centerY + DEADZONE) return "North";
+    if (y < centerY - DEADZONE) return "South";
   }
 
   return "wind error";
